73ac5eb43d027b79b0c80122f6b2bb3bdbb8c079
minghaozhi <597575122@qq.com>
2018-01-29 17:15:47 +0800

    逆向工程

--- shop-core/shop-core-generator/src/main/java/com/generator/util/GeneratorSqlmap.java
+++ shop-core/shop-core-generator/src/main/java/com/generator/util/GeneratorSqlmap.java
@@ -1,15 +1,17 @@
+package com.generator.util;
 
 
-import java.io.File;
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
 
 import org.mybatis.generator.api.MyBatisGenerator;
 import org.mybatis.generator.config.Configuration;
 import org.mybatis.generator.config.xml.ConfigurationParser;
-import org.mybatis.generator.exception.XMLParserException;
 import org.mybatis.generator.internal.DefaultShellCallback;
+
+import java.applet.AppletContext;
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+
 
 public class GeneratorSqlmap {
 
@@ -18,7 +20,8 @@ public class GeneratorSqlmap {
 		List<String> warnings = new ArrayList<String>();
 		boolean overwrite = true;
 		//指定 逆向工程配置文件
-		File configFile = new File("generatorConfig.xml"); 
+
+		File configFile = new File("E:\\wo3\\ssm-shop\\shop-core\\shop-core-generator\\src\\main\\resources\\generatorConfig.xml");
 		ConfigurationParser cp = new ConfigurationParser(warnings);
 		Configuration config = cp.parseConfiguration(configFile);
 		DefaultShellCallback callback = new DefaultShellCallback(overwrite);
