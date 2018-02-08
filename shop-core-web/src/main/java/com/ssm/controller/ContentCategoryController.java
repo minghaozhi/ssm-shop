990035c0adffceba02d245d0f841543f22f04f0f
minghaozhi <597575122@qq.com>
2018-02-08 16:06:06 +0800

    1111

--- shop-core-web/src/main/java/com/ssm/controller/ContentCategoryController.java
+++ shop-core-web/src/main/java/com/ssm/controller/ContentCategoryController.java
@@ -1,6 +1,14 @@
 package com.ssm.controller;
 
+import com.content.service.ContentCategoryService;
+import com.ssm.common.pojo.EasyUITreeNode;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.List;
 
 /**
  * @author 墨殇
@@ -9,4 +17,15 @@ import org.springframework.stereotype.Controller;
  */
 @Controller
 public class ContentCategoryController {
+    @Autowired
+    private ContentCategoryService contentCategoryService;
+
+    @RequestMapping("/content/category/list")
+    @ResponseBody
+    public List<EasyUITreeNode> getContentList(@RequestParam(name = "id",defaultValue = "0") Long parentId){
+
+        List<EasyUITreeNode> list=contentCategoryService.getContentList(parentId);
+        return list;
+    }
+
 }
